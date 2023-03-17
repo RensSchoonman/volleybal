88708c12ccf4d7d5c8e2c4e54dee21bc353d960b
Rens Schoonman <100193437+RensSchoonman@users.noreply.github.com>
2023-03-17 18:22:02 +0100

    Add files via upload

---
+++ volleybal.js
@@ -0,0 +1,23 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+const wrapper = document.getElementById("bubble-wrapper");
+
+const animateBubble = x => {  
+  const bubble = document.createElement("div");
+  
+  bubble.className = "bubble";
+  bubble.style.left = `${x}px`;
+  
+  wrapper.appendChild(bubble);
+  
+  setTimeout(() => wrapper.removeChild(bubble), 2000);
+}
+
+window.onmousemove = e => animateBubble(e.clientX);
+
